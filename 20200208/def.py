bd1e6fc5d931d638c7ff0a103682f284f63b65cd
kevin3182 <kimgm2022@tciscommunity.com>
2020-02-08 17:00:54 +0900

    if

---
+++ 20200208/def.py
@@ -0,0 +1,17 @@
+def add3(a):
+    res = a + 3
+    print(res)
+
+    return res
+add3(5)
+
+def div(a,b):
+    return a/b
+
+print(div(6,2))
+
+
+def mul4(star):
+    return star * 4
+
+print(mul4(10))
