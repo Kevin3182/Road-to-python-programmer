d58899ddc33149292fa75399be30f1865de90713
kevin3182 <kimgm2022@tciscommunity.com>
2020-02-15 15:43:05 +0900

    sync

---
+++ 20200204/For.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+universities = ['SNU', 'KAIST']
+
+for number in numbers:
+    print(number)
+
+for university in universities:
+    print(university)
---
+++ 20200204/Range.py
@@ -0,0 +1,5 @@
+for i in range(5):
+    print(i)
+
+for _ in range(5):
+    print(1)
